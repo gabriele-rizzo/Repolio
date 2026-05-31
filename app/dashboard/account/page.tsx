bd48d4c7e616306b3925f8865ebb2be873502e98
Gabriele Rizzo <gabrielerizzo.pers@gmail.com>
2026-05-31 18:07:05 +0200

    checkpoint

---
+++ app/dashboard/account/page.tsx
@@ -0,0 +1,116 @@
+import { authorize } from "@/actions/auth/authorize";
+import { PlatformBadge } from "@/components/platform-badge";
+import { Typo } from "@/components/typography";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { dateFormatRelative } from "@/lib/date/format-relative";
+import { prisma } from "@/lib/prisma";
+import { userInitials } from "@/lib/user/initials";
+import { Pen, Trash } from "lucide-react";
+import { Fragment } from "react";
+
+export default async function AccountPage() {
+    const client = await authorize();
+    const [nreports, nsnapshots, connections] = await Promise.all([
+        prisma.report.count({
+            where: { snapshots: { some: { client_id: client.id } } },
+        }),
+        prisma.snapshot.count({
+            where: { client_id: client.id },
+        }),
+        prisma.accountConnection.findMany({
+            where: { client_id: client.id },
+        }),
+    ]);
+
+    return (
+        <div className="space-y-4">
+            <Card className="px-4">
+                <button className="relative w-fit group">
+                    <Avatar className="size-24 group-hover:opacity-75">
+                        <AvatarFallback className="text-2xl">{userInitials(client.name)}</AvatarFallback>
+                    </Avatar>
+
+                    <div className="absolute size-7 flex items-center justify-center bg-muted bottom-0.5 right-0.5 border-4 border-card rounded-full">
+                        <Pen className="size-3 group-hover:opacity-50" />
+                    </div>
+                </button>
+
+                <div>
+                    <Typo as="title">{client.name}</Typo>
+                    <Typo as="muted">{client.email}</Typo>
+                </div>
+
+                <div className="h-20 bg-muted border flex flex-row">
+                    <div className="flex-1 flex p-4 justify-center flex-col">
+                        <Typo as="large">{nreports}</Typo>
+                        <Typo as="muted">{nreports === 1 ? "Report" : "Reports"}</Typo>
+                    </div>
+
+                    <Separator orientation="vertical" />
+
+                    <div className="flex-1 flex p-4 justify-center flex-col">
+                        <Typo as="large">{nsnapshots}</Typo>
+                        <Typo as="muted">{nsnapshots === 1 ? "Snapshot" : "Snapshots"}</Typo>
+                    </div>
+
+                    <Separator orientation="vertical" />
+
+                    <div className="flex-1 flex p-4 justify-center flex-col">
+                        <Typo as="large">{1}</Typo>
+                        <Typo as="muted">{1 === 1 ? "Managed Account" : "Managed Accounts"}</Typo>
+                    </div>
+                </div>
+            </Card>
+
+            <div className="space-y-3">
+                <Typo as="muted" className="text-xs uppercase tracking-wide font-medium">
+                    Account Connections
+                </Typo>
+
+                <div className="border">
+                    {connections.map((connection, index, list) => (
+                        <Fragment key={connection.id}>
+                            <div className="pl-4 pr-2.5 py-2.5 space-y-2 relative group">
+                                <Button
+                                    variant="destructive"
+                                    className="not-group-hover:hidden not-group-hover:pointer-events-none absolute top-2 right-2 size-6"
+                                >
+                                    <Trash />
+                                </Button>
+
+                                <PlatformBadge platform={connection.platform} />
+
+                                <div className="flex flex-row justify-between items-start">
+                                    <div className="flex flex-col gap-1">
+                                        <div className="flex flex-row gap-1.5">
+                                            <Typo as="muted">Account ID:</Typo>
+                                            <Typo as="normal" className="text-sm">
+                                                {connection.external_id}
+                                            </Typo>
+                                        </div>
+
+                                        {connection.expires_at && (
+                                            <div className="flex flex-row gap-1.5">
+                                                <Typo as="muted">Expires at:</Typo>
+                                                <Typo as="normal" className="text-sm">
+                                                    {dateFormatRelative(connection.expires_at)}
+                                                </Typo>
+                                            </div>
+                                        )}
+                                    </div>
+
+                                    <Typo as="muted">{dateFormatRelative(connection.created_at)}</Typo>
+                                </div>
+                            </div>
+
+                            {index !== list.length - 1 && <Separator orientation="horizontal" />}
+                        </Fragment>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
