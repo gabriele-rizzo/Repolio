d123f2aa27c99efc83db2b0d686edd043062a46a
Gabriele Rizzo <gabrielerizzo.pers@gmail.com>
2026-07-30 19:46:57 +0200

    logo

--- components/brand.tsx
+++ components/brand.tsx
@@ -8,8 +8,8 @@ type BrandProps = { label: string; href: `/${string}` } | { label?: undefined; h
 export function Brand({ label, href }: BrandProps) {
     return (
         <Link href={href ?? "/"} className={cn("flex flex-row gap-2", !label && !href && "p-2 hover:bg-muted")}>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary scheme-dark dark:scheme-light text-sidebar-primary-foreground">
-                <Image src={Logo} alt="logo" width={64} height={64} className="size-6" />
+            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-[#b7d9bb] text-sidebar-primary-foreground">
+                <Image src={Logo} alt="logo" width={64} height={64} className="size-8" />
             </div>
 
             <div className="grid flex-1 text-left text-sm leading-tight items-center">
